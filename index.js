297dfc40872392075f966161923cca0e404d3347
Andrea <anhe0001@stud.kea.dk>
2023-06-13 16:47:28 +0200

    js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 window.addEventListener("load", sidenVises);
+
 function sidenVises() {
   console.log("sidenVises");
-  //Skjul andre skærme
-  document.querySelector("#game_over").classList.add("hide");
-  document.querySelector("#level_complete").classList.add("hide");
-  //Vis start skærm
-  document.querySelector("#start").classList.remove("hide");
-  //Klik på start_knap
-  document.querySelector("#start_knap").addEventListener("click", startGame);
+  document.querySelector("#flyv").classList.add("flyv");
+  document.querySelector("#flyv_2").classList.add("flyv_2");
 }
